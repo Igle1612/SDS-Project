2d7d55fde972022e9de82ff9b810241e1b3731d7
Arnau Gris Garcia <blazegammer24@gmail.com>
2023-05-08 22:00:39 +0200

    Update mininet.py

--- mininet.py
+++ mininet.py
@@ -1,1 +1,37 @@
+from mininet.net import Mininet
+from mininet.topo import SingleSwitchTopo
 
+# Create a network with a single switch and 4 hosts
+net = Mininet(topo=SingleSwitchTopo(4))
+
+# Start the network
+net.start()
+
+# Open an xterm window for each host
+for host in net.hosts:
+    host.cmd('xterm &')
+
+# Wait for the user to close the xterm windows
+input('Press enter to shutdown the network...')
+
+# Stop the network
+net.stop()
+
+from mininet.net import Mininet
+from mininet.topo import SingleSwitchTopo
+
+# Create a network with a single switch and 4 hosts
+net = Mininet(topo=SingleSwitchTopo(4))
+
+# Start the network
+net.start()
+
+# Open an xterm window for each host
+for host in net.hosts:
+    host.cmd('xterm &')
+
+# Wait for the user to close the xterm windows
+input('Press enter to shutdown the network...')
+
+# Stop the network
+net.stop()
